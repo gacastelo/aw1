4149f81b4c3c84d5921efb2d9656d612311e5516
gacastelo <gabriel.castelo5949@gmail.com>
2025-09-08 16:03:26 -0300

    site a

--- aula04/processos/processar_cadastro.php
+++ aula04/processos/processar_cadastro.php
@@ -1,39 +1,3 @@
-<<<<<<< HEAD
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
- 
-$dados = json_decode(file_get_contents('../db/usuarios.json'), true);
-
-foreach ($dados as $usuario) {
-    if ($usuario['email'] === $_POST['email']) {
-        echo "Este e-mail já está cadastrado!";
-        exit;
-    }
-}
-
-
-if (empty($_POST['nome']) || empty($_POST['email']) || empty($_POST['senha'])) {
-        echo "Todos os campos devem ser preenchidos!";
-        exit;
-}
-
-
-
-$novoUsuario = [
-    'id' => count($dados) + 1,
-    'nome' => $_POST['nome'],
-    'email' => $_POST['email'],
-    'senha' => password_hash($_POST['senha'], PASSWORD_DEFAULT)
-];
-
-$dados[] = $novoUsuario;
-
-file_put_contents("../db/usuarios.json", json_encode($dados, JSON_PRETTY_PRINT));
-
-header('Location: ../login.php');
-exit;
-}
-=======
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
  
@@ -60,5 +24,4 @@ file_put_contents("../db/usuarios.json", json_encode($dados, JSON_PRETTY_PRINT))
 header('Location: ../login.php');
 exit;
 }
->>>>>>> aa7f53ac28380df673373e24f794e43c5de7462e
 ?>
--- aula04/processos/processar_login.php
+++ aula04/processos/processar_login.php
@@ -1,42 +1,3 @@
-<<<<<<< HEAD
-<?php 
-session_start();
-
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    if (empty($_POST['email']) || empty($_POST['senha'])) {
-        $_SESSION['status'] = "Todos os campos devem ser preenchidos!";
-        exit;
-    }
-    $email = $_POST['email'];
-    $senhaDigitada = $_POST['senha'];
-
-
-    $dados = json_decode(file_get_contents("../db/usuarios.json"), true);
-
-    foreach ($dados as $usuario) {
-        if ($usuario["email"] == $email) {
-            if (password_verify($senhaDigitada, $usuario["senha"])) {
-                // Senha correta
-                $_SESSION['email'] = $email;
-                $_SESSION['nome'] = $usuario['nome'];
-                $_SESSION['logado'] = true;
-                header('Location: ../formulario.php');
-                exit;
-            } else {
-                $_SESSION['logado'] = false;
-                $_SESSION['status'] = 'Senha incorreta';
-                exit;
-            }
-        }
-    }
-
-    $_SESSION['status'] = "Email não encontrado!";
-    exit;
-} else {
-    header('Location: ../login.php');
-    exit;
-}
-=======
 <?php 
 session_start();
 
@@ -69,5 +30,4 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
     header('Location: ../login.php');
     exit;
 }
->>>>>>> aa7f53ac28380df673373e24f794e43c5de7462e
 ?>
